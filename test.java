b8e63eef618e86d61f2db47d8000f04549d659bc
Ceren Ç <36920692+cerencelebi@users.noreply.github.com>
2018-04-15 00:43:56 +0300

    come342ProjectHw2Group7

---
+++ test.java
@@ -0,0 +1,31 @@
+package project342;
+import java.util.ArrayList;
+
+import project342.Campaign;
+import project342.Client;
+public class test {
+
+	
+	public static void main(String []args) 
+	
+	{
+		  
+		  
+		  Campaign campaign1=new Campaign(3, "Bilgisayar", "Mayis", "Haziran", 1500);
+		  Campaign campaign2=new Campaign(4, "Telefon", "Nisan", "Temmuz", 1300);
+		  
+	      ArrayList<Campaign> campaignlist1 = new ArrayList<Campaign>();
+		  campaignlist1.add(campaign1);
+		  
+		  ArrayList<Campaign> campaignlist2 = new ArrayList<Campaign>();
+	      campaignlist2.add(campaign2);
+		  
+	      Client client1 = new Client(1, "celebiler", "kızıltoprak", "ceren",campaignlist1);
+		  Client client2 = new Client(2, "sanverler", "Acibadem", "onur",campaignlist2);
+	      ArrayList<Client> client = new ArrayList<Client>();
+	      client.add(client1);
+	      client.add(client2); 
+	      
+	      
+	}
+}
